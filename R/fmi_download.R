7895711398fc5d296782950dbef54424bd3df221
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2017-10-07 23:29:38 +0300

    updated doc

--- R/fmi_download.R
+++ R/fmi_download.R
@@ -3,23 +3,23 @@
 #' Download data from the fmi weather API.
 #'
 #' @param fmi_apikey Your personal fmi-apikey.
-#'   See \url{http://en.ilmatieteenlaitos.fi/open-data-manual}.
+#'  See \url{http://en.ilmatieteenlaitos.fi/open-data-manual}.
 #' @param start Start time as a POSIXt-object obtained with e.g.
-#'   lubridate::as_datetime().
+#'  lubridate::as_datetime().
 #' @param end End time as a POSIXt-object. Defaults to \code{start}.
 #' @param station_id The weather station id. Defaults to \code{100971}
-#'   (Kaisaniemi weather station). The available stations can be downloaded
-#'   with the \link{get_stations}-function.
+#'  (Kaisaniemi weather station). A list of the available stations can be
+#'  downloaded with the \link{get_stations}-function.
 #' @param params Query parameters. If not provided, temprature and
-#'   precipitation is downloaded. See
-#'   \url{http://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services}.
+#'  precipitation is downloaded. See
+#'  \url{http://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services}.
 #' @param hourly If \code{TRUE}, hourly data is downloaded. Otherwise the daily
-#'   data is downloaded.
+#'  data is downloaded.
 #' @param simplify_names If \code{TRUE}, an attempt is made to simplify some of
-#'   the names of the variables. For example, t2m/tday is temp etc.
+#'  the names of the variables. For example, t2m/tday is temp etc.
 #'
 #' @return A tibble with date (& time if hourly data is requested) in the first
-#'   column and the variables specified in \code{params} in the other columns.
+#'  column and the variables specified in \code{params} in the other columns.
 #'
 #' @export
 #'
